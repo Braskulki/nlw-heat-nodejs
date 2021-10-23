5c0af3852a3b57e71c263d88d80f1d026df408f5
Yuri Braskulki <yuribraskulki@hotmail.com>
2021-10-23 16:19:52 -0300

    feat: messages socket.io

--- src/controllers/CreateMessageController.ts
+++ src/controllers/CreateMessageController.ts
@@ -1,4 +1,4 @@
-import { request, Request, Response } from "express";
+import { Request, Response } from "express";
 import { CreateMessageService } from "../services/CreateMessageService";
 
 class CreateMessageController {
